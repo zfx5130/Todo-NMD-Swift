b9166bf4b774c72b09a30bc4532efe2446905388
thomas <dui1cuo@126.com>
2016-06-12 14:44:01 +0800

    [Mod]minor fix

--- Todo-MND-Swift/Todo-MND-Swift/ViewController.swift
+++ Todo-MND-Swift/Todo-MND-Swift/ViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
+struct Vector2D {
+    var x = 0.0, y = 0.0
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        var toBeDoubled = Vector2D(x: 1.0, y: 4.0)
+        let afterDoubling = +++toBeDoubled
+        print(afterDoubling)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
+prefix operator +++ {}
+prefix func +++ (inout vector: Vector2D) -> Vector2D {
+    vector += vector
+    return vector
+}
+
+func += (inout left: Vector2D, right: Vector2D) {
+    left = left + right
+}
+
+func + (left: Vector2D, right: Vector2D) -> Vector2D {
+    return Vector2D(x: left.x + right.x, y: left.y + right.y)
+}
